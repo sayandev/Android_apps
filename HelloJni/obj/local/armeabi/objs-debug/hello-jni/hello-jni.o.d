c261e70a797d3fa55b9d8f2fdcdee04a79d2799c
sayandev <sayanmaity.10@gmail.com>
2015-03-26 10:17:59 -0400

    JavaNative

---
+++ HelloJni/obj/local/armeabi/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ /android-ndk-r8d/platforms/android-3/arch-arm/usr/include/string.h \
+ /android-ndk-r8d/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /android-ndk-r8d/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /android-ndk-r8d/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /android-ndk-r8d/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /android-ndk-r8d/platforms/android-3/arch-arm/usr/include/jni.h
+
+/android-ndk-r8d/platforms/android-3/arch-arm/usr/include/string.h:
+
+/android-ndk-r8d/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/android-ndk-r8d/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/android-ndk-r8d/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/android-ndk-r8d/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/android-ndk-r8d/platforms/android-3/arch-arm/usr/include/jni.h:
